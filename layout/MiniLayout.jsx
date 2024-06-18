65c60e464ad8cedb0a55d970936c62030de266e6
JivanaBam <jivanabam99@gmail.com>
2024-06-18 12:30:52 +0545

    path arranged

--- layout/MiniLayout.jsx
+++ layout/MiniLayout.jsx
@@ -2,11 +2,13 @@ import { Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import CustomSnackbar from "../component/CustomSnackbar";
+import Login from "../pages/Login";
 
 const MiniLayout = () => {
   return (
     <>
       <CustomSnackbar />
+      <Login />
       <Stack
         sx={{
           height: "100vh",
